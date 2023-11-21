9d4142a673b80ad80cdcbb5ebf55ab99a2303098
yangtory <wjdduscldrn@naver.com>
2023-11-21 16:14:18 +0900

    Java 프로젝트 시작

---
+++ Hello/src/Hello.java
@@ -0,0 +1,9 @@
+
+public class Hello {
+	public static void main(String[] args) {
+			System.out.println("Hello Korea");
+			System.out.println(30 + 40);
+			System.out.println("Korea");
+	}
+
+}
---
+++ Hello_001/src/com/callor/hello/HelloB.java
@@ -0,0 +1,19 @@
+/* 슬래스 2개로 시작되는 문장은 comment라고 한다
+ * 
+ * 문장이 시작될때 슬래시 2개로 시작되거나
+ * 슬래시 별표로 시작된 여러라인의 문장은
+ * Comment 라고한다.
+ * Comment 는 Java가 무시한다.
+ * comment 문장은 Java 가 공백과 같이 취급한다.
+ */
+// 나의 첫번째 자바 파일
+package com.callor.hello;
+
+public class HelloB {
+	// 자바 파일에서 제일 중요한 시작 블럭
+	public static void main(String[] args) {
+		// 안녕하세요 라는 문자열을 console 에 출력하는 코드
+		System.out.println("안녕하세요");
+	}
+
+}
