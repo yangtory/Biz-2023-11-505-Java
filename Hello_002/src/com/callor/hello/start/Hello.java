c6a5d19d428f25fc4c74bc070dd70b68bc29692c
yangtory <wjdduscldrn@naver.com>
2023-11-22 13:33:47 +0900

    오전수업

--- Hello_002/src/com/callor/hello/start/Hello.java
+++ Hello_002/src/com/callor/hello/start/Hello.java
@@ -1,5 +1,15 @@
 package com.callor.hello.start;
 
 public class Hello {
+	public static void main(String[] args) {
+		System.out.println(30 + 40);
+		System.out.println(30 - 40);
+		System.out.println(30 * 40);
+		System.out.println(30 / 40);
+		System.out.println(30 % 40);
+		
+		System.out.println(3000000000l + 4000000000l);
+		System.out.println(300000000l * 400000000l);
+	}
 
 }
