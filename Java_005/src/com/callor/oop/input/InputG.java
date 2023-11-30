4a44f56172a9addd4450c445a5dc334409458be2
yangtory <wjdduscldrn@naver.com>
2023-11-30 20:15:16 +0900

    inputG 수정

--- Java_005/src/com/callor/oop/input/InputG.java
+++ Java_005/src/com/callor/oop/input/InputG.java
@@ -2,6 +2,8 @@ package com.callor.oop.input;
 
 import java.util.Scanner;
 
+import com.callor.oop.utils.Line;
+
 public class InputG {
 
 	public static void main(String[] args) {
@@ -9,11 +11,14 @@ public class InputG {
 		Scanner scan = new Scanner(System.in);
 
 		while (true) {
-			System.out.println("정수 (QUIT:종료) >> ");
+			Line.dLine(30);
+			System.out.print("정수 (QUIT:종료) >> ");
+			
 			String str = scan.nextLine();
-			if (str.equals("QUIT"))
-				break;
-
+			if (str.equals("QUIT")) {
+				System.out.println("퇴근!");
+				break;				
+			}
 			try {
 				num = Integer.valueOf(str);
 			} catch (Exception e) {
@@ -21,18 +26,21 @@ public class InputG {
 				System.out.println("입력한 값 : " + num);
 				continue;
 			}
+			// 2 이상 출력 
 			if (num < 2) {
 				System.out.println("값은 2 이상 입력하세요");
 				System.out.println("입력한 값 : " + num);
 				continue;
 			}
+			// 짝수 검사 
 			if (num % 2 == 0) {
 				System.out.println("짝수입니다");
 				System.out.println("입력한 값 : " + num);
 			} else {
 				System.out.println("짝수가 아닙니다");
-				continue;
-			}
+				System.out.println("입력한 값 : " + num);
+			}	
+			// 소수 검사  
 			int i = 0;
 			for (i = 2; i < num ; i++) {
 				if (num % i == 0) {
@@ -40,10 +48,11 @@ public class InputG {
 				}
 			}
 			if (i < num) {
-				System.out.printf("  %d 는 소수가 아님\n", num);
+				System.out.printf(" %d 는 소수가 아닙니다\n", num);
 			} else {
-				System.out.printf("  %d 는 소수임\n", num);
+				System.out.printf(" %d 는 소수입니다\n", num);
 			}
-		}
-	}
+			
+		}// end while
+	}//end main
 }
